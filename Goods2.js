50fffdae69ccbef320442a80c7e5349cfbeefdf2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 21:59:31 +0100

    added in Goods2 new prop - sale

--- Goods2.js
+++ Goods2.js
@@ -1,8 +1,9 @@
 
 
 class Goods2 extends Goods {
-    constructor(name,price,image,count){
+    constructor(name,price,image,count,sale){
         super(name,price,image,count)
+        this.sale = false;
     }
 
 }
