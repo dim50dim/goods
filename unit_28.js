1a3063600bc06fe68a2cc99481a10741bacfa14b
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 21:57:49 +0100

    now we have three cards with products

--- unit_28.js
+++ unit_28.js
@@ -9,7 +9,7 @@ console.log(goods_1);
 
 const goods_2 = new Goods('watermelon',88, '653252_watermelon_food_fruit_meal_sweet_icon.png',20)
     // И теперь выведем на страницу
-const goods_3 = new Goods2('peach','1898849_fruit_peach_icon.png',55);
+const goods_3 = new Goods2('peach',22,'1898849_fruit_peach_icon.png',55);
 
 document.querySelector('.out-3').append(goods_1.draw());
 document.querySelector('.out-4').append(goods_2.draw());
