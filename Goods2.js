c8047cade4995f3d18b205cba78108c3fd13a3cf
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 22:06:18 +0100

    added condition - SALE - NOT SALE

--- Goods2.js
+++ Goods2.js
@@ -3,13 +3,17 @@
 class Goods2 extends Goods {
     constructor(name,price,image,count,sale){
         super(name,price,image,count)
-        this.sale = false;
+        this.sale = sale;
     }
       draw() {
         const div = super.draw();
 if (this.sale) {
 	let s = document.createElement('h2');
 	s.textContent = 'SALE';
+	div.append(s);
+}else{
+    let s = document.createElement('h2');
+	s.textContent = 'WE WILL WAIT YOU NEXT TIME';
 	div.append(s);
 }
 return div;
--- unit_28.js
+++ unit_28.js
@@ -8,7 +8,7 @@
 // console.log(goods_1);
 
 // const goods_2 = new Goods('watermelon',88, '653252_watermelon_food_fruit_meal_sweet_icon.png',20)
-//     // И теперь выведем на страницу
+//     
 // const goods_3 = new Goods2('peach',22,'1898849_fruit_peach_icon.png',55);
 
 // document.querySelector('.out-3').append(goods_1.draw());
@@ -20,3 +20,7 @@
 // const goods_2 = new Goods2('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400, true );
 // console.log(goods_2);
 
+const goods_7 = new Goods2('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400, false);
+console.log(goods_7);
+
+document.querySelector('.out-7').append(goods_7.draw());
