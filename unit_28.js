f1bcd0c9b1d945add6b7b01c85aeae26ebf57b37
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 21:39:58 +0100

    now we can see name of product , price and image

--- unit_28.js
+++ unit_28.js
@@ -3,3 +3,8 @@
 // const goods_1 = new Goods('apple',15.5);
 // const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400);
 // console.log(goods_1);
+
+const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400 );
+console.log(goods_1);
+// И теперь выведем на страницу
+document.querySelector('.out-3').append(goods_1.draw());
