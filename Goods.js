399522522702a7a5bfe5706b5020a0dae2ecd99a
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 21:38:16 +0100

    create method draw in class with print on page

--- Goods.js
+++ Goods.js
@@ -5,4 +5,26 @@ class Goods {
         this.image = image;
         this.count = count;
     }
+    draw(){
+       const div = document.createElement('div');
+       div.classList.add('goods');
+       
+       const head1 = document.createElement('h1');
+       head1.textContent = this.name;
+
+       const paragraph = document.createElement('p');
+       paragraph.classList.add('price');
+       paragraph.textContent = this.price;
+
+       const image = document.createElement('img');
+       image.src = this.image;
+       image.alt = this.name;
+
+       div.append(head1);
+       div.append(paragraph);
+       div.append(image)
+
+       return div;
+    }
+
 }
