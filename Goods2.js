3d598edec43d989ebef4ea2525f3d500e5aa81fb
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 21:53:58 +0100

    create new child class from Goods

---
+++ Goods2.js
@@ -0,0 +1,8 @@
+
+
+class Goods2 extends Goods {
+    constructor(name,price,image,count){
+        super(name,price,image,count)
+    }
+
+}
--- unit_28.js
+++ unit_28.js
@@ -9,5 +9,9 @@ console.log(goods_1);
 
 const goods_2 = new Goods('watermelon',88, '653252_watermelon_food_fruit_meal_sweet_icon.png',20)
     // И теперь выведем на страницу
+const goods_3 = new Goods2('peach','1898849_fruit_peach_icon.png',55);
+
 document.querySelector('.out-3').append(goods_1.draw());
-document.querySelector('.out-4').append(goods_2.draw())
+document.querySelector('.out-4').append(goods_2.draw());
+
+document.querySelector('.out-5').append(goods_3.draw())
