ff3e5f122806a77dc615c9203bb332739f2a9279
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-27 22:01:11 +0100

    make a check point with new features

--- unit_28.js
+++ unit_28.js
@@ -4,14 +4,18 @@
 // const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400);
 // console.log(goods_1);
 
-const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400 );
-console.log(goods_1);
+// const goods_1 = new Goods('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400 );
+// console.log(goods_1);
 
-const goods_2 = new Goods('watermelon',88, '653252_watermelon_food_fruit_meal_sweet_icon.png',20)
-    // И теперь выведем на страницу
-const goods_3 = new Goods2('peach',22,'1898849_fruit_peach_icon.png',55);
+// const goods_2 = new Goods('watermelon',88, '653252_watermelon_food_fruit_meal_sweet_icon.png',20)
+//     // И теперь выведем на страницу
+// const goods_3 = new Goods2('peach',22,'1898849_fruit_peach_icon.png',55);
 
-document.querySelector('.out-3').append(goods_1.draw());
-document.querySelector('.out-4').append(goods_2.draw());
+// document.querySelector('.out-3').append(goods_1.draw());
+// document.querySelector('.out-4').append(goods_2.draw());
 
-document.querySelector('.out-5').append(goods_3.draw())
+// document.querySelector('.out-5').append(goods_3.draw())const goods_2 = new Goods2('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400, true );
+
+
+const goods_2 = new Goods2('apple', 23.5, 'https://cdn0.iconfinder.com/data/icons/fruity-3/512/Apple-48.png', 400, true );
+console.log(goods_2);
